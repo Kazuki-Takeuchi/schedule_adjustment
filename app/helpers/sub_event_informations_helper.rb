1cefcb4b3c7124ec2f41309b7fbb4b694a3c5952
Kazuki Takeuchi <k.take57@gmail.com>
2018-02-05 01:19:01 +0900

    場所のリンクを調整

--- app/helpers/sub_event_informations_helper.rb
+++ app/helpers/sub_event_informations_helper.rb
@@ -1,2 +1,9 @@
 module SubEventInformationsHelper
+  def get_place_link(sub_event_information)
+    unless sub_event_information.url.blank?
+      link_to sub_event_information.place, sub_event_information.url, target: ["_blank"]
+    else
+      sub_event_information.place
+    end
+  end
 end
