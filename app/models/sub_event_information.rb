71dfffe979aa308544a56846104533482c66d3c9
Kazuki Takeuchi <k.take57@gmail.com>
2018-02-07 23:55:29 +0900

    urlチェック追加。不要なnotice 削除

--- app/models/sub_event_information.rb
+++ app/models/sub_event_information.rb
@@ -1,6 +1,8 @@
 class SubEventInformation < ApplicationRecord
   belongs_to :event_information
 
+  validates :url, { allow_blank: true, format: /\A#{URI::regexp(%w(http https))}\z/ }
+
   def get_only_start_time
     self.start_time.strftime("%H:%M:%S")
   end
