49bd02697fd914583e3d03186dee91b4973453f2
Kazuki Takeuchi <k.take57@gmail.com>
2018-02-10 01:37:53 +0900

    複数サブイベント参加者を重複カウントしないようにした

--- app/models/event_information.rb
+++ app/models/event_information.rb
@@ -2,6 +2,8 @@ class EventInformation < ApplicationRecord
   has_many :sub_event_informations
   
   def attendee_count
-    self.sub_event_informations.map{|info| info.attendances.count}.sum
+    self.sub_event_informations.map{|info|
+      info.attendances.map{|attendance|attendance.user_id}
+    }.flatten.uniq.length
   end
 end
