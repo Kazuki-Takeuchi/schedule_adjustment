32656d1c0a0b4437d306d137c8f74b7dedc6af00
Kazuki Takeuchi <k.take57@gmail.com>
2018-05-28 01:28:42 +0900

    卒業年追加 参加状態を見やすくした

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,4 +1,8 @@
 module AttendancesHelper
+  def get_attend_state_id(current_user, sub_event_information)
+    "attend_state_#{current_user.id}_#{sub_event_information.id}"
+  end
+
   def get_attend_tag_id(current_user, sub_event_information)
     "attend_#{current_user.id}_#{sub_event_information.id}"
   end
