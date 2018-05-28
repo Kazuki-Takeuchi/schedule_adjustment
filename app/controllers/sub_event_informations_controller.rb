4c95ce61fef4cf01ac3b54338a15c745e620ef78
Kazuki Takeuchi <k.take57@gmail.com>
2018-05-29 00:32:05 +0900

    参加ユーザを卒業年でソート

--- app/controllers/sub_event_informations_controller.rb
+++ app/controllers/sub_event_informations_controller.rb
@@ -4,7 +4,7 @@ class SubEventInformationsController < ApplicationController
   before_action :set_sub_event_information, only: %i(show edit update destroy)
 
   def show
-    @attendances = @sub_event_information.attendances.page(params[:page])
+    @attendances = @sub_event_information.attendances.includes(:user).order("users.graduation_year").page(params[:page])
   end
 
   def new
